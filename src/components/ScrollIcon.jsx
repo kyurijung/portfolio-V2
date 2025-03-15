08b6b2e574815da03798fda76ccd88849749785c
Kyuri Jung <kyuri.jg@gmail.com>
2025-03-15 12:50:19 -0400

    fix ident spacing

--- src/components/ScrollIcon.jsx
+++ src/components/ScrollIcon.jsx
@@ -4,26 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 
 function ScrollIcon() {
-    const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 400) {
-                setIsVisible(false);
-            } else {
-                setIsVisible(true);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 400) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    return (
-      <a href="#about" className={`absolute left-1/2 -translate-x-1/2 top-[90%] transition-opacity duration-100 ${isVisible ? "opacity-100" : "opacity-0"}`}>
-          <FontAwesomeIcon icon={faAnglesDown} className="fa-3x text-[#1A274B] hover:text-[#A94D0F] transition" />
-      </a>
-    );
+  return (
+    <a href="#about" className={`absolute left-1/2 -translate-x-1/2 top-[90%] transition-opacity duration-100 ${isVisible ? "opacity-100" : "opacity-0"}`}>
+      <FontAwesomeIcon icon={faAnglesDown} className="fa-3x text-[#1A274B] hover:text-[#A94D0F] transition" />
+    </a>
+  );
 }
 
 export default ScrollIcon;
