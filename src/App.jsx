40898a3a479b5006e57a01d488e93c774ddfc862
Kyuri Jung <kyuri.jg@gmail.com>
2025-03-15 11:39:09 -0400

    Initial Page Template

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "tailwindcss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "/src/components/Header";
+import NavBar from "/src/components/NavBar";
+import Home from "/src/pages/Home";
+
+function App() {
+	return (
+		<Router>
+			<div className="flex flex-col">
+				<Header></Header>
+				<NavBar></NavBar>
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</main>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
