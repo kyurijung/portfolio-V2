202126e70c02ba2c75299432ccaa8ce0b7fcb55c
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-09 21:27:08 -0400

    change caption

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className="pt-60">
           <h1 className="ml-20 leading-none text-[25px] font-mono font-bold text-[#1A274B]">Hi, my name is</h1>
           <h1 className="ml-18 leading-none text-[130px] font-extrabold text-[#1A274B]">Kyuri Jung.</h1>
-          <h1 className="ml-20 mt-4 leading-none text-[30px] font-normal text-[#A94D0F]">Computer Science Student @CarletonU</h1>
+          <h1 className="ml-20 mt-6 leading-none text-[30px] font-normal text-[#A94D0F]">Full-Stack Developer & Designer</h1>
         </div>
         <ScrollIcon></ScrollIcon>
       </div>
