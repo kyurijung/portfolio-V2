3e571d39646302a1bf1ec788eefc7a0142cda336
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-18 14:30:40 -0400

    lowercase header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ function Header() {
     <header className="fixed flex justify-center items-center h-18 w-full border-b bg-[#d8afe9] border-[#1A274B]">
       <nav>
         <ul className="space-x-20 font-mono font-bold text-[#1A274B]">
-          <a href="#about" className="hover:text-[#A94D0F] transition">00. About Me</a>
-          <a href="#experience" className="hover:text-[#A94D0F] transition">01. Experience</a>
-          <a href="#projects" className="hover:text-[#A94D0F] transition">02. Projects</a>
+          <a href="#about" className="hover:text-[#A94D0F] transition">00. about me</a>
+          <a href="#experience" className="hover:text-[#A94D0F] transition">01. experience</a>
+          <a href="#projects" className="hover:text-[#A94D0F] transition">02. projects</a>
         </ul>
       </nav>
     </header>
