e0bb421d0d55ee28394c8ef4939fbbe964ce3db6
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-20 17:54:39 -0400

    change footer color to beige

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -240,7 +240,7 @@ function Home() {
         </div>
       </div>
 
-      <div id="footer" className="pt-5 h-25 flex justify-end bg-blue-200">
+      <div id="footer" className="pt-5 h-25 flex justify-end">
         <hr className="absolute left-1/2 -translate-x-1/2 w-[75%]"></hr>
         <p className="absolute mt-3 pr-[13%] text-sm text-[#1A274B]">
           © 2025 <a href="https://github.com/kyurijung/kyurijung.github.io" className="underline">kyurijung.github.io</a> by Kyuri Jung
