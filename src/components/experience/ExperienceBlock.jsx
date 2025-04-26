41c4dcb27d47e722e7ac2fecdfc3504aef029f95
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-26 13:43:49 -0400

    dynamic render for experience section

---
+++ src/components/experience/ExperienceBlock.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from "@fortawesome/free-solid-svg-icons"
+
+ExperienceBlock.propTypes = {
+  company: PropTypes.string.isRequired,
+  url: PropTypes.number.isRequired,
+  roles: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+function ExperienceBlock({
+  company,
+  url,
+  roles
+}) {
+  return (
+    <div>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 text-primary hover:text-secondary transition leading-none"
+      >
+        <h2 className="text-[25px] font-bold">{company}</h2>
+        <FontAwesomeIcon icon={faLink} size="lg" />
+      </a>
+
+      {roles.map((role, index) => (
+        <div key={index} className="mt-2">
+          <div className="flex items-baseline justify-between w-225">
+            <h3 className="text-[20px] font-bold text-secondary">{role.title}</h3>
+            <h3 className="text-[18px] font-medium text-primary">{role.dates}</h3>
+          </div>
+
+          <ul className="ml-4 mt-2 space-y-1 list-disc pl-5 w-213">
+            {role.points.map((point, pointIndex) => (
+              <li key={pointIndex} className="text-[16px] text-primary">
+                {point.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ExperienceBlock;
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,145 +1,22 @@
 import data from "/src/data/experience";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLink } from "@fortawesome/free-solid-svg-icons"
+import ExperienceBlock from "/src/components/experience/ExperienceBlock";
 
 function Experience() {
   return (
-    <div id="experience" className="scroll-mt-18 bg-blue-200 pt-9 pb-9">
-      <div className="">
-        <h2 className="ml-40 leading-none text-[40px] font-bold text-primary">01. experience</h2>
-        <div>
-          <div className="ml-48 mt-7">
-            {/* Experience 1 */}
-            <a
-              href={data.experience[0].url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 text-primary hover:text-secondary transition"
-            >
-              <h2 className="text-[25px] font-bold">{data.experience[0].company}</h2>
-              <FontAwesomeIcon icon={faLink} size="lg" />
-            </a>
-            <div className="flex w-225 items-baseline justify-between">
-              <h3 className="text-[20px] font-bold text-secondary">Software Developer I</h3>
-              <h3 className="text-[18px] font-medium text-primary">January 2024 - Present</h3>
-            </div>
-            <ul className="list-disc ml-4 mt-2 space-y-1 pl-5">
-              <li className="text-primary mr-73 text-[16px]">
-                Optimized HBO Max app's accessibility and player controls by developing and refactoring the You.i One Engine's
-                React Native and Swift API, boosting stability and performance by 30%
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Automated acceptance testing prior to product deployment by implementing integration and unit tests, effectively
-                reducing 40% of manual testing
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Published documentation about new or upgraded features with specifications and guidelines, for 600+ engineers to
-                reference during future development
-              </li>
-            </ul>
-            {/* Experience 2 */}
-            <div className="flex w-225 items-baseline justify-between">
-              <h3 className="text-[20px] font-bold text-secondary">Software Developer</h3>
-              <h3 className="text-[18px] font-medium text-primary">May 2022 - August 2022</h3>
-            </div>
-            <ul className="list-disc ml-4 mt-2 space-y-1 pl-5">
-              <li className="text-primary mr-73 text-[16px]">
-                Optimized HBO Max app's accessibility and player controls by developing and refactoring the You.i One Engine's
-                React Native and Swift API, boosting stability and performance by 30%
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Automated acceptance testing prior to product deployment by implementing integration and unit tests, effectively
-                reducing 40% of manual testing
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Published documentation about new or upgraded features with specifications and guidelines, for 600+ engineers to
-                reference during future development
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          {/* Experience 3 */}
-          <div className="ml-48 mt-7">
-            <a
-              href={data.experience[1].url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 text-primary hover:text-secondary transition"
-            >
-              <h2 className="text-[25px] font-bold">{data.experience[1].company}</h2>
-              <FontAwesomeIcon icon={faLink} size="lg" />
-            </a>
-            <div className="flex w-225 items-baseline justify-between">
-              <h3 className="text-[20px] font-bold text-secondary">Web Content Developer</h3>
-              <h3 className="text-[18px] font-medium text-primary">January 2021 - April 2021</h3>
-            </div>
-            <ul className="list-disc ml-4 mt-2 space-y-1 pl-5">
-              <li className="text-primary mr-73 text-[16px]">
-                Customized client-side web parts and their layouts for the organization's intranet by using SPFx, CSS, and
-                third-party widgets, increasing site engagement for 200+ employees
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Migrated content from the former site onto the renewed SharePoint platform by sorting and filtering relevant
-                subjects, expediting transfer speeds by 2x
-              </li>
-              <li className="text-primary mr-73 text-[16px]">
-                Hosted live tutorial sessions demonstrating the creating and editing of site pages, training 30+ web editors
-                to adopt the new platform
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          {/* Experience 4 */}
-          <div className="ml-48 mt-7">
-            <a
-              href={data.experience[2].url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 text-primary hover:text-secondary transition"
-            >
-              <h2 className="text-[25px] font-bold">{data.experience[2].company}</h2>
-              <FontAwesomeIcon icon={faLink} size="lg" />
-            </a>
-            <div className="flex w-225 items-baseline justify-between">
-              <h3 className="text-[20px] font-bold text-secondary">Lead Designer</h3>
-              <h3 className="text-[18px] font-medium text-primary">September 2021 - April 2022</h3>
-            </div>
-            <ul className="list-disc ml-4 mt-2 space-y-1 pl-5">
-              <li className="text-primary mr-73 text-[16px]">
-                Produced graphics adhering to GDSC's brand theme by using Photoshop and Canva to generate templates and posts
-                for social media, targeting students interested in tech
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          {/* Experience 5 */}
-          <div className="ml-48 mt-7">
-            <a
-              href={data.experience[2].url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 text-primary hover:text-secondary transition"
-            >
-              <h2 className="text-[25px] font-bold">{data.experience[2].company}</h2>
-              <FontAwesomeIcon icon={faLink} size="lg" />
-            </a>
-            <div className="flex w-225 items-baseline justify-between">
-              <h3 className="text-[20px] font-bold text-secondary">Student Mentor</h3>
-              <h3 className="text-[18px] font-medium text-primary">August 2020 - April 2021</h3>
-            </div>
-            <ul className="list-disc ml-4 mt-2 space-y-1 pl-5">
-              <li className="text-primary mr-73 text-[16px]">
-                Mentored incoming women in Computer Science with supportive tutoring and valuable career insights, guiding them
-                through a successful first year in the program
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div id="experience" className="scroll-mt-18 pt-10 pb-10">
+      <h2 className="ml-40 mb-10 text-[40px] font-bold text-primary leading-none">{data.sectionName}</h2>
+
+      <section className="ml-48 space-y-10">
+        {data.experiences.map((experience, index) => (
+          <ExperienceBlock
+            key={index}
+            company={experience.company}
+            url={experience.url}
+            roles={experience.roles}
+          />
+        ))}
+      </section>
     </div>
   );
 }
