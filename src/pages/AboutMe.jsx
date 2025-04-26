64f5b6cdee57b5f49a8a686e216779461b3f06de
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-25 23:11:54 -0400

    cleanup aboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,20 +4,19 @@ import ImageOutline from "/src/components/home/ImageOutline";
 
 function AboutMe() {
   return (
-    <div id="about" className="scroll-mt-18 bg-amber-100 pt-9 pb-13">
-      <div className="">
-        <h2 className="ml-40 leading-none text-[40px] font-bold text-primary">{data.heading}</h2>
-        <div className="flex flex-row ml-48 mt-13 w-225 items-center justify-between">
-          <div className="w-120">
-            <text className="text-primary text-[18px]">
-              {data.career}
-              <br />
-              <br />
-              {data.hobbies}
-            </text>
-          </div>
-          <ImageOutline imageName="Portrait" width={19} />
+    <div id="about" className="scroll-mt-18 pt-10 pb-15">
+      <h2 className="ml-40 mb-7 text-[40px] font-bold text-primary leading-none">{data.sectionName}</h2>
+
+      <div className="ml-48 flex items-center justify-between w-225">
+        <div className="w-120">
+          <text className="text-[18px] text-primary">
+            {data.career}
+            <br />
+            <br />
+            {data.hobbies}
+          </text>
         </div>
+        <ImageOutline imageName={data.imageName} width={19} />
       </div>
     </div>
   );
