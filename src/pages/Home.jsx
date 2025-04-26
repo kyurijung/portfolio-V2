78653422a72639a3627ee9b72ba68a790c44d816
Kyuri Jung <kyuri.jg@gmail.com>
2025-04-25 23:17:51 -0400

    cleanup title section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
   const home = data.home
 
   return (
-    <div className="h-screen">
-      <div className="mt-[18%] leading-none">
+    <div className="h-screen pt-[18%]">
+      <div className="leading-none">
         <h1 className="ml-[8%] text-[25px] font-mono font-bold text-primary">{home.greeting}</h1>
         <h1 className="ml-[7.5%] text-[130px] font-extrabold text-primary">{home.name}</h1>
         <h1 className="ml-[8%] mt-6 text-[30px] font-normal text-secondary">{home.title}</h1>
